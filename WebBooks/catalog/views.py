94db177a8f3be6a81f361e7b69a14e6e385def42
m4ydd4y <danmayak@yandex.ru>
2024-10-11 00:23:17 +0300

    ЛЗ13 - работа на паре

--- WebBooks/catalog/views.py
+++ WebBooks/catalog/views.py
@@ -16,11 +16,14 @@ def index(request):
     num_authors = Author.objects.count()
     # Отрисовка HTML-шаблона index.html с данными
     # внутри переменной context
+    num_visits = request.session.get('num_visits', 0)     
+    request.session['num_visits'] = num_visits + 1 
     return render(request, 'index.html', context={
     'num_books': num_books,
     'num_instances': num_instances,
     'num_instances_available': num_instances_available,
-    'num_authors': num_authors}) 
+    'num_authors': num_authors,
+    'num_visits': num_visits}) 
 
 class BookDetailView(generic.DetailView):
     model = Book
