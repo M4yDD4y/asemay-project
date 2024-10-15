59fcc6ff7debca3cfcae6ccec17ad95b699af3af
Your Name <you@example.com>
2024-10-15 16:05:25 +0300

    ПЗ10 - работа на паре

--- WebBooks/catalog/views.py
+++ WebBooks/catalog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse 
 from .models import Book, Author, BookInstance, Genre
 from django.views import generic
- 
+from django.contrib.auth.mixins import LoginRequiredMixin
  
 # Create your views here. 
 def index(request):
@@ -35,3 +35,15 @@ class BookListView(generic.ListView):
 class AuthorListView(generic.ListView):
     model = Author
     paginate_by = 4
+
+class LoanedBooksByUserListView(LoginRequiredMixin, generic.ListView):
+ """
+ Универсальный класс представления списка книг,
+ находящихся в заказе у текущего пользователя.
+ """
+ model = BookInstance
+ template_name = 'catalog/bookinstance_list_borrowed_user.html'
+ paginate_by = 10
+ def get_queryset(self):
+    BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='2').order_by('due_back')
+    return
