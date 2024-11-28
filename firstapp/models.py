95a4679a593661fe5a33250ceb4f507a9489268d
m4ydd4y <danmayak@yandex.ru>
2024-11-29 01:40:24 +0300

    ЛЗ17

--- firstapp/models.py
+++ firstapp/models.py
@@ -2,17 +2,33 @@ from django.db import models
 
 # Create your models here.
 
-class Course(models.Model): 
+# class Course(models.Model): 
+#     name = models.CharField(max_length=30) 
+ 
+# class Student(models.Model):     
+#     name = models.CharField(max_length=30)     
+#     courses = models.ManyToManyField(Course) 
+
+# class User(models.Model): 
+#     name = models.CharField(max_length=20) 
+ 
+# class Account(models.Model):     
+#     login = models.CharField(max_length=20)     
+#     password = models.CharField(max_length=20) 
+#     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True) 
+
+class Person(models.Model):     
+    name = models.CharField(max_length=20) 
+    age = models.IntegerField()     
+    object_person = models.Manager() 
+    DoesNotExist = models.Manager 
+ 
+class Company(models.Model): 
     name = models.CharField(max_length=30) 
  
-class Student(models.Model):     
-    name = models.CharField(max_length=30)     
-    courses = models.ManyToManyField(Course) 
+class Product(models.Model): 
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)     
+    name = models.CharField(max_length=30) 
+    price = models.IntegerField() 
 
-class User(models.Model): 
-    name = models.CharField(max_length=20) 
- 
-class Account(models.Model):     
-    login = models.CharField(max_length=20)     
-    password = models.CharField(max_length=20) 
-    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True) 
+
